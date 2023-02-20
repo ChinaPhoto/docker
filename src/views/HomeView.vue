2884b5ecf950e3f69ae2e48d02950dff3f491259
feng_chen <feng_chen@intsig.net>
2023-02-20 18:32:11 +0800

    测试改动

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>这不是傻逼嘛</div>
   </div>
 </template>
 
